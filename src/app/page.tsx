d8aa1db939f9408371f2e9599b0e9e7823a62a37
jin <chkftm12@gmail.com>
2023-03-23 08:47:55 +0900

    - add: main hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <section className="bg-gray-500 w-full">Main</section>
+    <>
+      <Hero />
+    </>
   )
 }
