6fd4c6856e9d3fd127ef2bfba6944d48afcdee17
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2023-03-23 08:49:39 +0900

    - add: main hero component (#3)
    * tailwind setup
    
    * style: add layout and styling
    
    * - add: main hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Here() {
+  return (
+    <section className='text-center'>
+      <Image
+        className='rounded-full mx-auto'
+        src="/images/my-profile.png"
+        alt="my-profile"
+        width={250}
+        height={300}
+      />
+      <h2 className='text-3xl font-bold mt-2'>{"Hi, I'm Jin"}</h2>
+      <h3 className='text-lx font-semibold'>Front-end Engineer</h3>
+      <p>누군가에게 도움이 될 수 있는 개발자로 성장 중입니다.</p>
+      <Link href="/contact" className='bg-yellow-500 px-3 py-1 rounded-xl font-bold text-sm mt-2'>Contact Me</Link>
+    </section>
+  )
+}
