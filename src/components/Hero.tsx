f374c84fc3caae02d91cdb812808dd3959b35b10
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2023-03-23 12:44:24 +0900

    update read posts (#4)
    * tailwind setup
    
    * style: add layout and styling
    
    * - add: main hero component
    
    * -feat: read featured posts
    
    -feat: add multi carousel posts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
 export default function Here() {
   return (
@@ -10,12 +9,16 @@ export default function Here() {
         src="/images/my-profile.png"
         alt="my-profile"
         width={250}
-        height={300}
+        height={250}
       />
       <h2 className='text-3xl font-bold mt-2'>{"Hi, I'm Jin"}</h2>
       <h3 className='text-lx font-semibold'>Front-end Engineer</h3>
       <p>누군가에게 도움이 될 수 있는 개발자로 성장 중입니다.</p>
-      <Link href="/contact" className='bg-yellow-500 px-3 py-1 rounded-xl font-bold text-sm mt-2'>Contact Me</Link>
+      <Link href="/contact">
+        <button className='bg-yellow-500 rounded-xl font-bold mt-2 px-3 py-1'>
+          Contact Me
+        </button>
+      </Link>
     </section>
   )
 }
