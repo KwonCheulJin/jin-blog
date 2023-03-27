0fcd021af2d4b222617f5cd7867a570d3bce9368
jin <chkftm12@gmail.com>
2023-03-27 20:49:38 +0900

    - feat: 개별 포스트 페이지로 이동 가능하도록 로직 구현

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,34 +1,28 @@
-import { Post } from '@/service/posts'
-import Image from 'next/image'
-import Link from 'next/link'
+import { Post } from '@/service/posts';
+import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
-  post: Post
-}
-export default function PostCard({post:{
-  title,
-  description,
-  date,
-  category,
-  path,
-}}:Props) {
+  post: Post;
+};
+export default function PostCard({ post: { title, description, date, category, path } }: Props) {
   return (
     <Link href={`/posts/${path}`}>
-      <article className='shadow-lg rounded-md overflow-hidden hover:shadow-xl'>
+      <article className="shadow-lg rounded-md overflow-hidden hover:shadow-xl">
         <Image
-          className='w-full'
+          className="w-full"
           src={`/images/posts/${path}.png`}
           alt={title}
           width={300}
           height={200}
         />
-        <div className='flex flex-col items-center p-4'>
-          <time className='self-end'>{date.toLocaleString()}</time>
-          <h3 className='text-lg font-bold'>{title}</h3>
-          <p className='w-full truncate text-center'>{description}</p>
-          <span className='bg-green-100 rounded-lg text-sm px-2 my-2'>{category}</span>
+        <div className="flex flex-col items-center p-4">
+          <time className="self-end">{date.toLocaleString()}</time>
+          <h3 className="w-full truncate text-center text-lg font-bold">{title}</h3>
+          <p className="w-full truncate text-center">{description}</p>
+          <span className="bg-green-100 rounded-lg text-sm px-2 my-2">{category}</span>
         </div>
       </article>
     </Link>
-  )
+  );
 }
