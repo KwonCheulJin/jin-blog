b68ba7cffbb0ebf8570c5db02d0b9d74fdd5097b
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2023-03-29 18:12:24 +0900

    release (#8)
    * tailwind setup
    
    * style: add layout and styling
    
    * - add: main hero component
    
    * -feat: read featured posts
    
    -feat: add multi carousel posts
    
    * - feat: posts 페이지 이동시 필터가능한 포스트가 보여지도록 추가
    
    * - feat: 개별 포스트 페이지로 이동 가능하도록 로직 구현
    
    * -feat: 개별 포스트 react-markdown, react-syntax-highlighter 사용 페이지 제작 완료
    
    * -feat: add AdjacentPostCard Component
    
    * - feat: about page 완료
    - feat: contact page style 완료

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -1,14 +1,13 @@
-import { getFeaturedPosts } from '@/service/posts'
-import PostsGrid from './PostsGrid'
-
+import { getFeaturedPosts } from '@/service/posts';
+import PostsGrid from './PostsGrid';
 
 export default async function FeaturedPosts() {
   const posts = await getFeaturedPosts();
 
   return (
-    <section>
-      <h2 className='text-2xl font-bold my-2'>Featured Posts</h2>
-      <PostsGrid posts={posts}/>
+    <section className="mx-8">
+      <h2 className="text-2xl font-bold my-2">Featured Posts</h2>
+      <PostsGrid posts={posts} />
     </section>
-  )
+  );
 }
