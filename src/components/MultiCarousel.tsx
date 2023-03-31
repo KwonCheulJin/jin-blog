1f414275bd537bb6ab2b1028c45c869e5cfd52b0
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2023-03-31 10:01:46 +0900

    release (#12)
    * tailwind setup
    
    * style: add layout and styling
    
    * - add: main hero component
    
    * -feat: read featured posts
    
    -feat: add multi carousel posts
    
    * - feat: posts 페이지 이동시 필터가능한 포스트가 보여지도록 추가
    
    * - feat: 개별 포스트 페이지로 이동 가능하도록 로직 구현
    
    * -feat: 개별 포스트 react-markdown, react-syntax-highlighter 사용 페이지 제작 완료
    
    * -feat: add AdjacentPostCard Component
    
    * - feat: about page 완료
    - feat: contact page style 완료
    
    * - feat: email 전송 기능 추가
    
    * - feat: metadata 설정
    
    * - feat: use Reat cache
    - feat: 일부 성능 개선 작업
    
    * - fix: 메인 케러셀 화면에 표시되지 않던 문제 수정

--- src/components/MultiCarousel.tsx
+++ src/components/MultiCarousel.tsx
@@ -1,73 +1,46 @@
-'use client'
+'use client';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
-import { Post } from '@/service/posts'
-import PostCard from './PostCard'
+import { Post } from '@/service/posts';
+import PostCard from './PostCard';
 
 type Props = {
-  posts: Post[]
-}
+  posts: Post[];
+};
 
-export default function MultiCarousel({posts}:Props) {
-
-  const postCardList = posts.map((post, index) => (
-    <PostCard
-      key={index}
-      post={post}
-    />
-  ))
+export default function MultiCarousel({ posts }: Props) {
+  const postCardList = posts.map((post, index) => <PostCard key={index} post={post} />);
 
   const responsive = {
     superLargeDesktop: {
-      breakpoint: { max: 1536, min: 1024 },
-      items: 4
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
     },
     desktop: {
-      breakpoint: { max: 1024, min: 768 },
+      breakpoint: { max: 3000, min: 1024 },
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 768, min: 640 },
+      breakpoint: { max: 1024, min: 464 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 640, min: 0 },
+      breakpoint: { max: 464, min: 0 },
       items: 1,
-    }
+    },
   };
 
   return (
     <Carousel
-      additionalTransfrom={0}
-      arrows
       autoPlay
       autoPlaySpeed={2000}
-      centerMode={false}
-      className=""
-      containerClass="container-with-dots"
-      dotListClass=""
-      draggable
-      focusOnSelect={false}
       infinite
       itemClass="m-2"
-      keyBoardControl
-      minimumTouchDrag={80}
       pauseOnHover
-      renderArrowsWhenDisabled={false}
-      renderButtonGroupOutside={false}
-      renderDotsOutside={false}
-      rewind={false}
-      rewindWithAnimation={false}
-      rtl={false}
-      shouldResetAutoplay
-      showDots={false}
-      sliderClass=""
-      slidesToSlide={2}
-      swipeable
       responsive={responsive}
     >
       {postCardList}
     </Carousel>
-  )
+  );
 }
