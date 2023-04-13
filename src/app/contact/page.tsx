8b0ad6f5a5489ff9af905b8c616b1090dd58c5d0
jin <chkftm12@gmail.com>
2023-04-13 18:00:04 +0900

    - fix: 전체 레이아웃 구성 수정

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,7 @@
 import ContactForm from '@/components/ContactForm';
-import { AiFillGithub } from 'react-icons/ai';
+import AnimatedText from '@/components/AnimatedText';
 import type { Metadata } from 'next';
+import Layout from '@/components/Layout';
 
 export const metadata: Metadata = {
   title: 'Contact',
@@ -9,21 +10,11 @@ export const metadata: Metadata = {
 
 export default function ContactPage() {
   return (
-    <section className="flex flex-col items-center">
-      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
-      <p>chkftm12@gmail.com</p>
-      <div>
-        <a
-          href="https://github.com/KwonCheulJin"
-          target="_blank"
-          rel="noreferrer"
-          className="text-5xl hover:text-yellow-500"
-        >
-          <AiFillGithub />
-        </a>
-      </div>
-      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
-      <ContactForm />
+    <section className="flex flex-col items-center w-full">
+      <Layout className="pt-12 flex flex-col items-center justify-center">
+        <AnimatedText text="Send me an email" className="pb-16" />
+        <ContactForm />
+      </Layout>
     </section>
   );
 }
--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -17,7 +17,7 @@ export default function Details({ position, company, companyLink, time, address,
   return (
     <li
       ref={ref}
-      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between"
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between"
     >
       <LiIcon reference={ref} />
       <motion.div
