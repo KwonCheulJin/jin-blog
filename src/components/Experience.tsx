d69a406d94e509995f578e0a4523c703d9060383
jin <chkftm12@gmail.com>
2023-04-13 18:02:50 +0900

    - fix: merge

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -12,11 +12,7 @@ export default function Experience() {
   });
 
   return (
-<<<<<<< HEAD
     <div className="my-64">
-=======
-    <div className="my-64 h-full">
->>>>>>> 71008eaebd6c692bfc75f00bf72328f60304875c
       <h2 className="font-bold text-8xl my-32 w-full text-center">Experience</h2>
       <div ref={ref} className="relative w-[75%] mx-auto">
         <motion.div
@@ -31,7 +27,6 @@ export default function Experience() {
             companyLink="www.reverschool.com"
             time="2022-present"
             address="Remote work"
-<<<<<<< HEAD
             work="리버스쿨(보안 교육 플랫폼)에서 프론트엔드를 맡아서 개발하고 있습니다."
           />
           <Details
@@ -41,61 +36,6 @@ export default function Experience() {
             time="2021-2022"
             address="교보문고 파견"
             work="교보문고 VCMS(Saas) 개발에 참여하였습니다."
-=======
-            work="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, ipsa adipisci
-              laboriosam velit sunt vel consequuntur, ad obcaecati voluptas facere eos animi.
-              Assumenda sed ex animi eligendi amet id debitis!"
-          />
-          <Details
-            position="Software Engineer"
-            company="opstech"
-            companyLink="www.reverschool.com"
-            time="2022-present"
-            address="Remote work"
-            work="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, ipsa adipisci
-              laboriosam velit sunt vel consequuntur, ad obcaecati voluptas facere eos animi.
-              Assumenda sed ex animi eligendi amet id debitis!"
-          />
-          <Details
-            position="Software Engineer"
-            company="opstech"
-            companyLink="www.reverschool.com"
-            time="2022-present"
-            address="Remote work"
-            work="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, ipsa adipisci
-              laboriosam velit sunt vel consequuntur, ad obcaecati voluptas facere eos animi.
-              Assumenda sed ex animi eligendi amet id debitis!"
-          />
-          <Details
-            position="Software Engineer"
-            company="opstech"
-            companyLink="www.reverschool.com"
-            time="2022-present"
-            address="Remote work"
-            work="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, ipsa adipisci
-              laboriosam velit sunt vel consequuntur, ad obcaecati voluptas facere eos animi.
-              Assumenda sed ex animi eligendi amet id debitis!"
-          />
-          <Details
-            position="Software Engineer"
-            company="opstech"
-            companyLink="www.reverschool.com"
-            time="2022-present"
-            address="Remote work"
-            work="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, ipsa adipisci
-              laboriosam velit sunt vel consequuntur, ad obcaecati voluptas facere eos animi.
-              Assumenda sed ex animi eligendi amet id debitis!"
-          />
-          <Details
-            position="Software Engineer"
-            company="opstech"
-            companyLink="www.reverschool.com"
-            time="2022-present"
-            address="Remote work"
-            work="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, ipsa adipisci
-              laboriosam velit sunt vel consequuntur, ad obcaecati voluptas facere eos animi.
-              Assumenda sed ex animi eligendi amet id debitis!"
->>>>>>> 71008eaebd6c692bfc75f00bf72328f60304875c
           />
         </ul>
       </div>
