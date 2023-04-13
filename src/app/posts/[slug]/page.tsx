e487fb635273bb234a500ce7b1e46105d22849c0
jin <chkftm12@gmail.com>
2023-04-13 18:06:26 +0900

    - fix: 개별 post 레이아웃 수정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import AdjacentPostCard from '@/components/AdjacentPostCard';
 import PostContent from '@/components/PostContent';
 import type { Metadata } from 'next';
+import Layout from '@/components/Layout';
 
 type Props = {
   params: {
@@ -25,20 +26,22 @@ export default function PostPage({ params: { slug } }: Props) {
   const post = getPostData(slug);
   const { title, path, next, prev } = post;
   return (
-    <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
-      <Image
-        className="w-full h-1/5 max-h-[500px]"
-        src={`/images/posts/${path}.png`}
-        alt={title}
-        width={760}
-        height={420}
-      />
-      <PostContent post={post} />
-      <section className="flex lg:flex-row max-md:flex-col max-sm:flex-col shadow-md">
-        {prev && <AdjacentPostCard post={prev} type="prev" />}
-        {next && <AdjacentPostCard post={next} type="next" />}
-      </section>
-    </article>
+    <Layout className="pt-16">
+      <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+        <Image
+          className="w-full h-1/5 max-h-[500px]"
+          src={`/images/posts/${path}.png`}
+          alt={title}
+          width={760}
+          height={420}
+        />
+        <PostContent post={post} />
+        <section className="flex lg:flex-row max-md:flex-col max-sm:flex-col shadow-md">
+          {prev && <AdjacentPostCard post={prev} type="prev" />}
+          {next && <AdjacentPostCard post={next} type="next" />}
+        </section>
+      </article>
+    </Layout>
   );
 }
 
