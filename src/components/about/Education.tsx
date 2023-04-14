789a43f3a0e41621ce02fc93609586bf1cd68919
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2023-04-14 20:27:04 +0900

    release (#20)
    * tailwind setup
    
    * style: add layout and styling
    
    * - add: main hero component
    
    * -feat: read featured posts
    
    -feat: add multi carousel posts
    
    * - feat: posts 페이지 이동시 필터가능한 포스트가 보여지도록 추가
    
    * - feat: 개별 포스트 페이지로 이동 가능하도록 로직 구현
    
    * -feat: 개별 포스트 react-markdown, react-syntax-highlighter 사용 페이지 제작 완료
    
    * -feat: add AdjacentPostCard Component
    
    * - feat: about page 완료
    - feat: contact page style 완료
    
    * - feat: email 전송 기능 추가
    
    * - feat: metadata 설정
    
    * - feat: use Reat cache
    - feat: 일부 성능 개선 작업
    
    * - fix: 메인 케러셀 화면에 표시되지 않던 문제 수정
    
    * - fix: Contact page form 너비 적용 문제 수정
    
    * - feat: json으로 post card 내용 가져오는 로직에서 md파일이 추가되면 포스트가 추가되는 방식으로 로직 변경
    
    * - fix: ci/cd build error fix
    
    * - add: opengraph image test
    
    * fix: 메일 화면 디자인 변경
    
    * - add: about page skills, experience add component
    
    * - fix: 전체 레이아웃 구성 수정
    
    * - fix: merge
    
    * - fix: 개별 post 레이아웃 수정
    
    * - add: 프론트엔드 생존코스 3기 1주차 주간회고 작성
    
    * - fix: 폴더 구조 변경
    - add: dark / light theme 추가
    - fix: 사용하지 않는 컴포넌트 삭제
    
    * - feat: 반응형 구현 완료
    - feat: 페이지 전환 시 트렌지션효과 추가

--- src/components/about/Education.tsx
+++ src/components/about/Education.tsx
@@ -9,18 +9,23 @@ export default function Education() {
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ['start end', 'center start'],
+    layoutEffect: false,
   });
 
   return (
-    <div className="my-64">
-      <h2 className="font-bold text-8xl my-32 w-full text-center">Education</h2>
-      <div ref={ref} className="relative w-[75%] mx-auto">
+    <div className="my-64 md:my-32">
+      <h2 className="font-bold text-8xl my-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Education
+      </h2>
+      <div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light
+          md:w-[2px] md:left-[30px] xs:left-[20px]
+          "
         />
 
-        <ul className="w-full flex flex-col items-start justify-between ml-4">
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <EducationDetails
             type="구공팩토리"
             time="2021-2021"
--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -12,10 +12,13 @@ export default function NewPostCard({ post: { title, date, path } }: Props) {
     <motion.li
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
-      className="relative w-full px-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:bg-dark dark:text-light dark:border-light"
+      className="relative w-full px-4 py-6 my-4 rounded-xl flex items-center
+      justify-between bg-light text-dark first:mt-0 border border-solid
+      border-dark border-r-4 border-b-4 dark:bg-dark dark:text-light dark:border-light
+      sm:flex-col sm:items-start"
     >
       <MovingImage title={title} path={path} />
-      <time className="text-primary dark:text-primaryDark font-semibold pl-4">
+      <time className="text-primary dark:text-primaryDark font-semibold pl-4 sm:self-start sm:pl-0 sm:pt-2 xs:text-sm">
         {date.toLocaleString('ko-KR', {
           year: 'numeric',
           month: 'numeric',
