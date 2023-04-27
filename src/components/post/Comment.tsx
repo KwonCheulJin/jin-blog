17441f53bed81162454bdf316b231628607ee058
jin <chkftm12@gmail.com>
2023-04-27 19:35:21 +0900

    - add: utteranc로 블로그 코맨트 연동

---
+++ src/components/post/Comment.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { useEffect, useRef } from 'react';
+
+export default function Comments() {
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const scriptElement = document.createElement('script');
+    scriptElement.async = true;
+    scriptElement.crossOrigin = 'anonymous';
+    scriptElement.src = 'https://utteranc.es/client.js';
+
+    scriptElement.setAttribute('issue-term', 'pathname');
+    scriptElement.setAttribute('label', 'comment');
+    scriptElement.setAttribute('repo', 'KwonCheulJin/jin-blog');
+    scriptElement.setAttribute('theme', 'preferred-color-scheme');
+
+    ref.current?.appendChild(scriptElement);
+  }, []);
+
+  return <div ref={ref} />;
+}
