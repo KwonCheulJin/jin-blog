c2a6f6350e7a703fb0e5ce553939837014edf8e9
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2023-05-03 22:32:27 +0900

    - fix: 약력 내용 변경 (#48)
    * tailwind setup
    
    * style: add layout and styling
    
    * - add: main hero component
    
    * -feat: read featured posts
    
    -feat: add multi carousel posts
    
    * - feat: posts 페이지 이동시 필터가능한 포스트가 보여지도록 추가
    
    * - feat: 개별 포스트 페이지로 이동 가능하도록 로직 구현
    
    * -feat: 개별 포스트 react-markdown, react-syntax-highlighter 사용 페이지 제작 완료
    
    * -feat: add AdjacentPostCard Component
    
    * - feat: about page 완료
    - feat: contact page style 완료
    
    * - feat: email 전송 기능 추가
    
    * - feat: metadata 설정
    
    * - feat: use Reat cache
    - feat: 일부 성능 개선 작업
    
    * - fix: 메인 케러셀 화면에 표시되지 않던 문제 수정
    
    * - fix: Contact page form 너비 적용 문제 수정
    
    * - feat: json으로 post card 내용 가져오는 로직에서 md파일이 추가되면 포스트가 추가되는 방식으로 로직 변경
    
    * - fix: ci/cd build error fix
    
    * - add: opengraph image test
    
    * fix: 메일 화면 디자인 변경
    
    * - add: about page skills, experience add component
    
    * - fix: 전체 레이아웃 구성 수정
    
    * - fix: merge
    
    * - fix: 개별 post 레이아웃 수정
    
    * - add: 프론트엔드 생존코스 3기 1주차 주간회고 작성
    
    * - fix: 폴더 구조 변경
    - add: dark / light theme 추가
    - fix: 사용하지 않는 컴포넌트 삭제
    
    * - feat: 반응형 구현 완료
    - feat: 페이지 전환 시 트렌지션효과 추가
    
    * - fix: 페이지 라우트 안되는 문제 수정
    
    * - fix: 버튼 컬러 및 애니메이션 추가, feature post 카드 에니메이션 및 스타일 변경
    
    * - add: google analitics 추가
    
    * - add: Analytics compoent in document
    
    * - fix: gtag script
    
    * - remove: Analitics component
    
    * - fix: post inline tag style change
    - add: 이전에 작성해 두었던 포스트 추가
    - fix: modify post type
    
    * - fix: CleanCode-13 첨부링크 틀린 부분 수정
    - fix: 클린코드 13과 끝 순서 변경을 위한 날짜 수정
    
    * - add: @vercel/analytics
    - add: google-site-verification metatag
    
    * - add: Analytics component
    
    * - add: sitemap, robots.ts
    
    * - fix: dark mode script position change
    - fix: 사용하지 않는 파일 정리
    
    * - fix: 운영체제가 다크모드 지정되어있을 때 화면이 다크모드로 변경되지 않았던 문제 수정
    
    * - add: new post(next.js 13 - seo)
    
    * - add: metatag keywords 노출 되도록 수정
    - add: 구글 에드센스 스크립트 추가
    
    * - add: ads.txt
    
    * - add: next script 추가 블로
    - fix: script 관리 컴포넌트로 분리
    
    * - fix: twitter link 수정
    - add: 2주차 회고 초안 작성
    
    * - add: 프론트엔드 생존코스 2주차 주간회고 작성
    
    * - fix: 다크모드 트렌지션이팩트 컬러 변경
    - fix: 반응형 스타일 일부 수정
    
    * - docs: 프로젝트 README.md 수정
    
    * - fix: feature post image height 조정
    
    * - fix: featured image size 변경
    
    * - fix: 웹 접근성용 aria-label 추가
    
    * - fix: 트랜지션 컬러 추가
    - docs: TIL 블로그 추가
    
    * - add: utteranc로 블로그 코맨트 연동
    
    * - docs: utterances 추가 블로그 글 추가
    
    * merge
    
    * merge2
    
    * - docs: 프론트앤드 생존코스 3기 3주차 회고 작성
    
    * - fix: 약력 내용 변경

--- src/components/about/Biography.tsx
+++ src/components/about/Biography.tsx
@@ -8,19 +8,26 @@ export default function Biography() {
           Biography
         </h2>
         <p className="font-medium">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, ipsa adipisci laboriosam
-          velit sunt vel consequuntur, ad obcaecati voluptas facere eos animi. Assumenda sed ex
-          animi eligendi amet id debitis!
+          긍정적인 마음가짐과 진정성을 가지고 개발을 하고 클라이밍을 좋아하는 프론트엔드 개발자
+          JIN이라고 합니다.
+          <br /> 개발은 구공팩토리 부트캠프를 통해서 처음 시작하게 되었습니다.
+          <br /> 첫 회사 프리픽스에서는 교보문고 VCMS(Video Content Management System)을 개발하는
+          프로젝트에 투입되었고 사용한 기술은 백엔드(Java)를 사용하였습니다.
+          <br /> 이곳에서는 기본적인 관리자 화면 CRUD 기능에 대한 작업을 진행했었습니다.
         </p>
         <p className="my-4 font-medium">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati cum laboriosam quas
-          molestiae voluptas itaque autem vitae, commodi iure ratione, distinctio esse veniam
-          facilis dolorum, molestias sint quis porro debitis!
+          프리픽스 이후 옵스테크로 회사를 이직 후에 이곳에서는 리버스쿨(보안 교육 플랫폼)
+          프론트엔드를 담당하고 있습니다.
+          <br /> 현재 사용하고 있는 기술 스팩은 React, Typescript, Redux, Redux-saga,
+          styled-components를 사용하여 개발을 하고있습니다.
+          <br /> 혼자서 프론트를 담당하고 있으면서 테스트코드의 중요성을 인식하고 현재 프로젝트에
+          적용하지 못해서 메가테라 프론트엔드 생존코스를 통해서 단위 테스트 및 E2E 테스트를
+          적용해보기 위해 공부중에 있습니다.
         </p>
         <p className="font-medium">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati cum laboriosam quas
-          molestiae voluptas itaque autem vitae, commodi iure ratione, distinctio esse veniam
-          facilis dolorum, molestias sint quis porro debitis!
+          저의 목표는 언제나 긍정적인 마인드와 지속적으로 React 및 프론트 관련 기술에 대한 전문
+          지식을 연마하는 데 중점을 두고 개발자로서 기술과 지식을 계속해서 성장시켜서 누군가를
+          도와줄 수 있는 개발자가 되는 것입니다.
         </p>
       </div>
       <div className="col-span-4 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light md:order-1 md:col-span-8">
