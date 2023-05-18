d5a49c3e1ecfd26f9030d520c338cf28af44dd34
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2023-05-18 17:43:50 +0900

    - feat: add main featured posts (#53)
    * tailwind setup
    
    * style: add layout and styling
    
    * - add: main hero component
    
    * -feat: read featured posts
    
    -feat: add multi carousel posts
    
    * - feat: posts 페이지 이동시 필터가능한 포스트가 보여지도록 추가
    
    * - feat: 개별 포스트 페이지로 이동 가능하도록 로직 구현
    
    * -feat: 개별 포스트 react-markdown, react-syntax-highlighter 사용 페이지 제작 완료
    
    * -feat: add AdjacentPostCard Component
    
    * - feat: about page 완료
    - feat: contact page style 완료
    
    * - feat: email 전송 기능 추가
    
    * - feat: metadata 설정
    
    * - feat: use Reat cache
    - feat: 일부 성능 개선 작업
    
    * - fix: 메인 케러셀 화면에 표시되지 않던 문제 수정
    
    * - fix: Contact page form 너비 적용 문제 수정
    
    * - feat: json으로 post card 내용 가져오는 로직에서 md파일이 추가되면 포스트가 추가되는 방식으로 로직 변경
    
    * - fix: ci/cd build error fix
    
    * - add: opengraph image test
    
    * fix: 메일 화면 디자인 변경
    
    * - add: about page skills, experience add component
    
    * - fix: 전체 레이아웃 구성 수정
    
    * - fix: merge
    
    * - fix: 개별 post 레이아웃 수정
    
    * - add: 프론트엔드 생존코스 3기 1주차 주간회고 작성
    
    * - fix: 폴더 구조 변경
    - add: dark / light theme 추가
    - fix: 사용하지 않는 컴포넌트 삭제
    
    * - feat: 반응형 구현 완료
    - feat: 페이지 전환 시 트렌지션효과 추가
    
    * - fix: 페이지 라우트 안되는 문제 수정
    
    * - fix: 버튼 컬러 및 애니메이션 추가, feature post 카드 에니메이션 및 스타일 변경
    
    * - add: google analitics 추가
    
    * - add: Analytics compoent in document
    
    * - fix: gtag script
    
    * - remove: Analitics component
    
    * - fix: post inline tag style change
    - add: 이전에 작성해 두었던 포스트 추가
    - fix: modify post type
    
    * - fix: CleanCode-13 첨부링크 틀린 부분 수정
    - fix: 클린코드 13과 끝 순서 변경을 위한 날짜 수정
    
    * - add: @vercel/analytics
    - add: google-site-verification metatag
    
    * - add: Analytics component
    
    * - add: sitemap, robots.ts
    
    * - fix: dark mode script position change
    - fix: 사용하지 않는 파일 정리
    
    * - fix: 운영체제가 다크모드 지정되어있을 때 화면이 다크모드로 변경되지 않았던 문제 수정
    
    * - add: new post(next.js 13 - seo)
    
    * - add: metatag keywords 노출 되도록 수정
    - add: 구글 에드센스 스크립트 추가
    
    * - add: ads.txt
    
    * - add: next script 추가 블로
    - fix: script 관리 컴포넌트로 분리
    
    * - fix: twitter link 수정
    - add: 2주차 회고 초안 작성
    
    * - add: 프론트엔드 생존코스 2주차 주간회고 작성
    
    * - fix: 다크모드 트렌지션이팩트 컬러 변경
    - fix: 반응형 스타일 일부 수정
    
    * - docs: 프로젝트 README.md 수정
    
    * - fix: feature post image height 조정
    
    * - fix: featured image size 변경
    
    * - fix: 웹 접근성용 aria-label 추가
    
    * - fix: 트랜지션 컬러 추가
    - docs: TIL 블로그 추가
    
    * - add: utteranc로 블로그 코맨트 연동
    
    * - docs: utterances 추가 블로그 글 추가
    
    * merge
    
    * merge2
    
    * - docs: 프론트앤드 생존코스 3기 3주차 회고 작성
    
    * - fix: 약력 내용 변경
    
    * - docs: 4주차 주간회고 작성 완료
    
    * - docs: 5주차 주간회고 작성
    
    * - fix: generate dynamic sitemap
    - update: next 13.4
    
    * - fix: sitemap default url 수정
    
    * - feat: add main featured posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero';
 import Layout from '@/components/common/Layout';
 import TransitionEffect from '@/components/common/TransitionEffect';
+import CarouselPosts from '@/components/post/CarouselPosts';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <TransitionEffect />
       <Layout className="pt-0 lg:pt-8 md:pt-4 sm:pt-0">
         <Hero />
+        <CarouselPosts />
       </Layout>
     </>
   );
