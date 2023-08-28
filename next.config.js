7d0e5a780c5b51a8b818cd38e73ce50cfaa0e79b
jin <chkftm12@gmail.com>
2023-08-28 21:58:57 +0900

    fix: about page

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  experimental: {
-    appDir: true,
-    scrollRestoration: true
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
