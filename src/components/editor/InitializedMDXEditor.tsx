506b4e5f47af01d60ea63b6031ed10992604c250
jin <chkftm12@gmail.com>
2023-11-14 22:56:21 +0900

    feat: add mdxeditor fix: customhook theme -> next-themes modify

---
+++ src/components/editor/InitializedMDXEditor.tsx
@@ -0,0 +1,138 @@
+'use client';
+
+import {
+  MDXEditor,
+  MDXEditorMethods,
+  diffSourcePlugin,
+  markdownShortcutPlugin,
+  AdmonitionDirectiveDescriptor,
+  DirectiveDescriptor,
+  directivesPlugin,
+  frontmatterPlugin,
+  headingsPlugin,
+  imagePlugin,
+  linkDialogPlugin,
+  linkPlugin,
+  listsPlugin,
+  quotePlugin,
+  tablePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  SandpackConfig,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  sandpackPlugin,
+  KitchenSinkToolbar,
+} from '@mdxeditor/editor';
+import { LeafDirective } from 'mdast-util-directive';
+import { useTheme } from 'next-themes';
+import { FC } from 'react';
+interface EditorProps {
+  markdown: string;
+  editorRef?: React.MutableRefObject<MDXEditorMethods | null>;
+}
+
+const defaultSnippetContent = `
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+`.trim();
+
+export const virtuosoSampleSandpackConfig: SandpackConfig = {
+  defaultPreset: 'react',
+  presets: [
+    {
+      label: 'React',
+      name: 'react',
+      meta: 'live react',
+      sandpackTemplate: 'react',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.js',
+      snippetLanguage: 'jsx',
+      initialSnippetContent: defaultSnippetContent,
+    },
+    {
+      label: 'React',
+      name: 'react',
+      meta: 'live',
+      sandpackTemplate: 'react',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.js',
+      snippetLanguage: 'jsx',
+      initialSnippetContent: defaultSnippetContent,
+    },
+    {
+      label: 'Virtuoso',
+      name: 'virtuoso',
+      meta: 'live virtuoso',
+      sandpackTemplate: 'react-ts',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.tsx',
+      initialSnippetContent: defaultSnippetContent,
+      dependencies: {
+        'react-virtuoso': 'latest',
+        '@ngneat/falso': 'latest',
+      },
+    },
+  ],
+};
+export async function expressImageUploadHandler(image: File) {
+  const formData = new FormData();
+  formData.append('image', image);
+  const response = await fetch('/uploads/new', { method: 'POST', body: formData });
+  const json = (await response.json()) as { url: string };
+  return json.url;
+}
+/**
+ * Extend this Component further with the necessary plugins or props you need.
+ * proxying the ref is necessary. Next.js dynamically imported components don't support refs.
+ */
+export default function InitializedMDXEditor({ markdown, editorRef }: EditorProps) {
+  const { theme } = useTheme();
+  const handleChange = (markdown: string) => {
+    console.log('🚀 ~ file: page.tsx:30 ~ handleChange ~ markdown:', markdown);
+  };
+  return (
+    <MDXEditor
+      className={`${
+        theme === 'dark' ? 'dark-theme dark-editor' : 'light-theme'
+      } border dark:border-gray-100 border-black`}
+      contentEditableClassName="prose dark:prose-invert"
+      onChange={handleChange}
+      ref={editorRef}
+      markdown={markdown}
+      plugins={[
+        toolbarPlugin({ toolbarContents: () => <KitchenSinkToolbar /> }),
+        listsPlugin(),
+        quotePlugin(),
+        headingsPlugin({ allowedHeadingLevels: [1, 2, 3] }),
+        linkPlugin(),
+        linkDialogPlugin(),
+        imagePlugin({
+          imageAutocompleteSuggestions: [
+            'https://via.placeholder.com/150',
+            'https://via.placeholder.com/150',
+          ],
+        }),
+        tablePlugin(),
+        thematicBreakPlugin(),
+        frontmatterPlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: 'txt' }),
+        sandpackPlugin({ sandpackConfig: virtuosoSampleSandpackConfig }),
+        codeMirrorPlugin({
+          codeBlockLanguages: { js: 'JavaScript', css: 'CSS', txt: 'text', tsx: 'TypeScript' },
+        }),
+        directivesPlugin({
+          directiveDescriptors: [AdmonitionDirectiveDescriptor],
+        }),
+        diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown: 'boo' }),
+        markdownShortcutPlugin(),
+      ]}
+    />
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
@@ -15,27 +13,21 @@ module.exports = {
         primary: '#B63E96', // 240,86,199
         primaryDark: '#58E6D9', // 80,230,217
       },
-      backgroundImage:{
+      backgroundImage: {
         circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 100px)',
-        circularDark: 'repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 8px, #1b1b1b 100px)',
+        circularDark:
+          'repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 8px, #1b1b1b 100px)',
         circularLightLg: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 80px)',
-        circularDarkLg: 'repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 80px)',
+        circularDarkLg:
+          'repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 80px)',
         circularLightMd: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 60px)',
-        circularDarkMd: 'repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 6px,#1b1b1b 60px)',
+        circularDarkMd:
+          'repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 6px,#1b1b1b 60px)',
         circularLightSm: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 40px)',
-        circularDarkSm: 'repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 4px,#1b1b1b 40px)',
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            '.prose :where(code):not(:where([class~="not-prose"] *))::before': {
-              'content': 'none'
-            },
-            '.prose :where(code):not(:where([class~="not-prose"] *))::after': {
-              'content': 'none'
-            }
-          },
-        },
+        circularDarkSm:
+          'repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 4px,#1b1b1b 40px)',
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
     screens: {
@@ -56,9 +48,7 @@ module.exports = {
 
       xs: { max: '479px' },
       // => @media (max-width: 479px) { ... }
-    }
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
