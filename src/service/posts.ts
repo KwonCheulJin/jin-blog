a1fe81c43e1c3404d503e89eb998a777a94eed8c
jin <chkftm12@gmail.com>
2023-11-21 19:18:33 +0900

    fix: lint error

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -21,19 +21,19 @@ const FOLDER = 'data/posts';
 
 export const getAllPosts = () => {
   const files = readdirSync(FOLDER);
-  const markdownPosts = files.filter((file) => file.endsWith('.md'));
+  const markdownPosts = files.filter(file => file.endsWith('.md'));
   const posts = markdownPosts
-    .map((fileName) => {
+    .map(fileName => {
       const fileContents = readFileSync(`${FOLDER}/${fileName}`, 'utf-8');
       const matterResult = matter(fileContents);
       return {
-        title: matterResult.data.title,
-        description: matterResult.data.description,
-        date: matterResult.data.date,
-        category: matterResult.data.category,
-        path: matterResult.data.path,
-        image: matterResult.data.image,
-        featured: matterResult.data.featured,
+        title: matterResult.data.title as string,
+        description: matterResult.data.description as string,
+        date: matterResult.data.date as Date,
+        category: matterResult.data.category as string,
+        path: matterResult.data.path as string,
+        image: matterResult.data.image as string,
+        featured: matterResult.data.featured as boolean,
       };
     })
     .sort((a, b) => (a.date > b.date ? -1 : 1));
@@ -42,17 +42,17 @@ export const getAllPosts = () => {
 };
 
 export function getFeaturedPosts(): Post[] {
-  return getAllPosts().filter((post) => post.featured);
+  return getAllPosts().filter(post => post.featured);
 }
 
 export function getNonFeaturedPosts(): Post[] {
-  return getAllPosts().filter((post) => !post.featured);
+  return getAllPosts().filter(post => !post.featured);
 }
 
 export function getPostData(fileName: string): PostData {
   const fileContents = readFileSync(`${FOLDER}/${fileName}.md`, 'utf-8');
   const posts = getAllPosts();
-  const post = posts.find((post) => post.path === fileName);
+  const post = posts.find(post => post.path === fileName);
   if (!post) {
     throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
   }
