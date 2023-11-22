e1319af240d9d850948385d68e07b3e03e38ddb6
jin <chkftm12@gmail.com>
2023-11-22 22:33:58 +0900

    feat: add editor image uploader function

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,11 @@
 import { SupabaseAdapter } from '@auth/supabase-adapter';
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import jwt from 'jsonwebtoken';
+import { Adapter } from 'next-auth/adapters';
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -18,7 +20,25 @@ const handler = NextAuth({
   adapter: SupabaseAdapter({
     url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
-  }),
-});
+  }) as Adapter,
+  callbacks: {
+    async session({ session, user }) {
+      const signingSecret = process.env.SUPABASE_JWT_SECRET;
+      if (signingSecret && user.type === 'HOST') {
+        const payload = {
+          aud: 'authenticated',
+          exp: Math.floor(new Date(session.expires).getTime() / 1000),
+          sub: user.id,
+          email: user.email,
+          role: 'authenticated',
+        };
+        session.supabaseAccessToken = jwt.sign(payload, signingSecret);
+      }
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
