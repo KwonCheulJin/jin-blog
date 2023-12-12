905d4a9b8e3d020d97e64817d44cf981c38bdc90
jin <chkftm12@gmail.com>
2023-12-12 14:51:54 +0900

    fix: 유저 타입 session에 추가되도록 로직 수정 fix: 포스트 스타일 일부 수정 fix: 기본 로그인 화면 수정

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,14 +1,17 @@
-'use client';
+import { cn } from '@/lib/utils';
 
 type Props = {
   children: React.ReactNode;
   className?: string;
 };
 
-export default function Layout({ children, className = '' }: Props) {
+export default function Layout({ children, className }: Props) {
   return (
     <div
-      className={`z-0 inline-block h-full w-full bg-light dark:bg-dark  ${className}`}
+      className={cn(
+        'z-0 inline-block h-full w-full bg-light dark:bg-dark',
+        className,
+      )}
     >
       {children}
     </div>
