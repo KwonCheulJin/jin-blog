f546b108df7ef8afbe30c6342d46d6bef0438780
jin <chkftm12@gmail.com>
2023-12-12 11:37:54 +0900

    fix: build error -2

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -19,7 +19,7 @@ export type PostData = PostDetail & {
 const baseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'http://localhost:3000';
+    : 'http://127.0.0.1:8000';
 
 export async function getPostDetail(id: string) {
   const data = await fetch(`${baseUrl}/api/post/${id}`, {
