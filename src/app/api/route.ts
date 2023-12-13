e26c13cb4be17e30f8fcd0dff1d1c76a7762b3c6
jin <chkftm12@gmail.com>
2023-12-13 16:35:32 +0900

    fix: build error

--- src/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-
-try {
-  // something that throws an error
-  throw new Error('Internal server error');
-} catch (e) {
-  // your should probably log the error as well
-  redirect('/error?status=500&message=servererror');
-}
