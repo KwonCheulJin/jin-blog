f536ddf4aad6652537d3df5e5ba860b07cd84751
jin <chkftm12@gmail.com>
2023-12-18 16:00:04 +0900

    fix: about exprience opstech link modified

--- src/fixtures/experience.ts
+++ src/fixtures/experience.ts
@@ -4,7 +4,7 @@ export const experiences: Experience[] = [
   {
     position: 'Software Engineer',
     company: 'opstech',
-    companyLink: 'www.reverschool.com',
+    companyLink: 'https://www.reverschool.com',
     time: '2022-present',
     address: 'Remote work',
     work: '리버스쿨(보안 교육 플랫폼)에서 프론트엔드를 맡아서 개발하고 있습니다.',
