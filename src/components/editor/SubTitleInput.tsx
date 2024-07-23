814618582edb37cf65be61dad75bc7fb90ec735b
jin <chkftm12@gmail.com>
2024-07-23 20:14:26 +0900

    fix: EditorContainer 조합 컴포넌트 네이밍 수정

--- src/components/editor/SubTitleInput.tsx
+++ src/components/editor/SubTitleInput.tsx
@@ -1,6 +1,6 @@
 import { usePostStore } from '@/store/post';
 
-export default function SubTitle() {
+export default function SubTitleInput() {
   const { addPost, setAddPost } = usePostStore();
   const handleSubTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
