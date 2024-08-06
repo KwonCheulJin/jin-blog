40fcc7b3e85ed74f5fcc130ef38c3b1afb801850
jin <chkftm12@gmail.com>
2024-08-06 09:20:06 +0900

    update: react-markdown, remark-gfm, rehype-raw 패키지 버전 업데이트 fix: react-markdown 버전 업데이트로 인한 일부 에러 코드 수정 작업

--- src/components/post/MarkdownViewer.tsx
+++ src/components/post/MarkdownViewer.tsx
@@ -1,10 +1,9 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { CodeProps } from 'react-markdown/lib/ast-to-react';
-import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import remarkGfm from 'remark-gfm';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import rehypeRaw from 'rehype-raw';
-import Image from 'next/image';
+import remarkGfm from 'remark-gfm';
 
 type Props = {
   content: string;
@@ -14,20 +13,14 @@ export default function MarkdownViewer({ content }: Props) {
   return (
     <ReactMarkdown
       className="prose max-w-none rounded-none font-medium dark:prose-invert prose-p:text-base prose-pre:bg-dark prose-pre:p-0 prose-li:text-base dark:text-light"
-      remarkPlugins={[[remarkGfm, { fence: true }]]}
+      remarkPlugins={[[remarkGfm]]}
       rehypePlugins={[rehypeRaw]}
       components={{
-        code({
-          node,
-          inline,
-          className,
-          children,
-          style,
-          ...props
-        }: CodeProps) {
+        code(props) {
+          const { children, className, node, ref, ...rest } = props;
           const title = className?.split(':')[1];
           const match = /language-(\w+)/.exec(className || '');
-          return !inline && match ? (
+          return match ? (
             <div>
               {title && (
                 <header className="overflow-x-scroll rounded-tl-sm rounded-tr-sm bg-zinc-700 px-3 py-5 font-mono font-bold">
@@ -35,11 +28,11 @@ export default function MarkdownViewer({ content }: Props) {
                 </header>
               )}
               <SyntaxHighlighter
-                style={oneDark}
-                language={match[1]}
+                {...rest}
                 PreTag="div"
                 className="!mt-0 !rounded-tl-none !rounded-tr-none"
-                {...props}
+                language={match[1]}
+                style={oneDark}
               >
                 {String(children).replace(/\n$/, '')}
               </SyntaxHighlighter>
