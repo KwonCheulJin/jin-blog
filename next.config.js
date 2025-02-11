a10439561a0f84bc851d048ebe06267ef3b308e0
jin <chkftm12@gmail.com>
2025-02-11 10:51:26 +0900

    vercel preview error fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 const nextConfig = {
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
