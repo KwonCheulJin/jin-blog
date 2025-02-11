d879e5e261f015bb2811d3719df77b4eef78ca16
jin <chkftm12@gmail.com>
2025-02-11 10:46:00 +0900

    next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 const nextConfig = {
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
