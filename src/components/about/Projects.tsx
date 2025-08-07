eadd349bfa1f24031b775245f23210d3eb055bf3
jin <chkftm12@gmail.com>
2025-08-07 17:40:02 +0900

    project card 사이즈 줄임

--- src/components/about/Projects.tsx
+++ src/components/about/Projects.tsx
@@ -14,7 +14,7 @@ export default function Projects() {
       <h2 className="mb-16 w-full text-center text-4xl font-bold md:text-6xl lg:my-32 lg:text-8xl">
         Projects
       </h2>
-      <div className="relative mx-auto w-[75%] md:w-[90%] lg:w-full">
+      <div className="relative mx-auto w-[75%] md:w-[80%]">
         <Carousel3D />
       </div>
     </div>
